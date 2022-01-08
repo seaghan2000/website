aba0468bca13d4a0e1e3b835a7696341ec0ae844
seaghan2000 <seaghan00@gmail.com>
2022-01-07 23:20:53 -0700

    working locally

---
+++ logger.js
@@ -0,0 +1,10 @@
+
+//Does nothing as of current
+var url = 'https://seaghan.herokuapp.com/'
+
+function log(message){
+    console.log(message);
+}
+
+module.exports.log = log;
+module.exports.endPoint = url;
