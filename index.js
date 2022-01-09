090526da9bed6ce276fc439d4e960d3c0b9b2b76
seaghan2000 <seaghan00@gmail.com>
2022-01-09 16:25:44 -0700

    set up for css

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/css', express.static(__dirname + 'public/img'))
+app.use('/css', express.static(__dirname + 'public/js'))
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -9,7 +16,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-//var app = express();
+
 
 //app.get('/', function(request, response){
  //   response.sendFile('index.html');
