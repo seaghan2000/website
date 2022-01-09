bfac294582f1ca93d85b6e0267131a61348b075a
seaghan2000 <seaghan00@gmail.com>
2022-01-09 16:38:09 -0700

    some css and ejs setup

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/css', express.static(__dirname + 'public/img'))
-app.use('/css', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
