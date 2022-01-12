f3d22b6d824bbd97aa56cc3e97a1345693a54cd9
seaghan2000 <seaghan00@gmail.com>
2022-01-12 16:57:50 -0700

    added new page for projects

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
+app.get('/projects', (req, res) => res.render('pages/projects'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
